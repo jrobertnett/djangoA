6c14093bfdee53f64c3f71c9d3d98796fd651067
jrobert <jrobertnet@gmail.com>
2017-08-18 20:39:58 -0600

    Primer Envio. Se creo el modelo del Blog. Se habilito el sitio de admin Se aplico un tema

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Publicar(models.Model):
+        autor = models.ForeignKey('auth.User')
+        titulo = models.CharField(max_length=200)
+        texto = models.TextField()
+        fecha_creacion = models.DateTimeField(
+                default=timezone.now)
+        fecha_publica = models.DateTimeField(
+                blank=True, null=True)
+
+        def publicacion(self):
+            self.fecha_publica = timezone.now()
+            self.save()
+
+        def __str__(self):
+            return self.title
