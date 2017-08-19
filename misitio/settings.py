62f286d546e46869b46b429d4d417cb9b35ea27f
jrobert <jrobertnet@gmail.com>
2017-08-18 21:37:35 -0600

    Se modifico el Host.

--- misitio/settings.py
+++ misitio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cy3xbsz2hvy4g14z^)q+7y)f4gkgm53i9i@s7@x%ah5-v8e*xz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jrobert.pythonanyware.com','127.0.0.1']
+ALLOWED_HOSTS = ['jrobert.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
