19aad022053e20d1f604931783d29a8633d33f67
jrobert <jrobertnet@gmail.com>
2017-10-08 18:01:02 -0600

    Borradores y login

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.listar_publicacion ),
+    url(r'^$', views.listar_publicacion, name='listar_publicacion' ),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detalle_pub, name = 'postear'),
     url(r'^postear/nuevo/$', views.nueva_publicacion, name='nueva_publicacion'),
     url(r'^postear/(?P<pk>[0-9]+)/editar/$', views.editar_publicacion, name='editar_publicacion'),
+    url(r'^borrador/$', views.borradores_publicacion, name= 'borradores_publicacion'),
+    url(r'^postear/(?P<pk>\d+)/Publicar/$',views.postear_publicacion, name= 'postear_publicacion'),
+    url(r'^postear/(?P<pk>\d+)/remover/$',views.remover_publicacion, name= 'remover_publicacion' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicar
 from.forms import PostearForm
+from django.contrib.auth.decorators import login_required
 
 def listar_publicacion(request):
     publi = Publicar.objects.filter(fecha_publica__lte=timezone.now()).order_by ('fecha_publica')
@@ -11,19 +12,21 @@ def detalle_pub(request, pk):
         p = get_object_or_404(Publicar, pk=pk)
         return render(request, 'blog/detalle_publicacion.html', {'p': p})
 
+@login_required
 def nueva_publicacion(request):
         if request.method == "POST":
             f = PostearForm(request.POST)
             if f.is_valid():
                 p = f.save(commit=False)
                 p.autor = request.user
-                p.fecha_publica = timezone.now()
+                #p.fecha_publica = timezone.now()
                 p.save()
                 return redirect('postear', pk=p.pk)
         else:
             f = PostearForm()
         return render(request, 'blog/nueva_publicacion.html', {'f': f})
 
+@login_required
 def editar_publicacion(request, pk):
         p = get_object_or_404(Publicar, pk=pk)
         if request.method == "POST":
@@ -36,3 +39,20 @@ def editar_publicacion(request, pk):
         else:
             f = PostearForm(instance=p)
         return render(request, 'blog/editar_publicacion.html', {'f': f})
+
+@login_required
+def borradores_publicacion(request):
+    f = Publicar.objects.filter(fecha_publica__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/borradores_publicacion.html', {'f': f})
+
+@login_required
+def postear_publicacion(request, pk):
+    p = get_object_or_404(Publicar, pk=pk)
+    p.publicacion()
+    return redirect('postear', pk=p.pk)
+
+@login_required
+def remover_publicacion(request, pk):
+    p = get_object_or_404(Publicar, pk=pk)
+    p.delete()
+    return redirect('listar_publicacion')
